dceb01617691b47a67d32a8326ac38acee0642e3
xylnx <xylnx@mailbox.org>
2022-07-30 21:19:21 +0200

    Log errors to console

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,12 +3,16 @@ require('dotenv').config;
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  console.log(req.headers);
+  console.log('###', req.headers);
+  console.log({ authHeader });
   if (!authHeader) return res.sendStatus(401);
   console.log(authHeader); // Bearer token
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); // invalid token, no access!
+    if (err) {
+      console.log(err);
+      return res.sendStatus(403); // invalid token, no access!
+    }
     req.user = decoded.username;
     next();
   });
