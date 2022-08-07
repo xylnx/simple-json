1bc376f8b99b9dd21764b768e42392dd1876af11
xylnx <xylnx@mailbox.org>
2022-08-07 19:49:54 +0200

    Adjust allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOriginis = [
   'http://127.0.0.1:8080',
   'http://localhost:8080',
-  'https://xylnx.github.io/fwl/',
+  'https://xylnx.github.io',
 ];
 
 module.exports = allowedOriginis;
